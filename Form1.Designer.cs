fd22d3a4ece3c279069f82f2f11d4cc638368de3
мирро <68013505+mirro-iw@users.noreply.github.com>
2021-01-29 12:41:05 +0100

    1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,7 +32,14 @@ namespace Plutonium_ban_bypass
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.computerName = new System.Windows.Forms.Label();
             this.machineGUID = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.newGUID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.weed = new System.Windows.Forms.Label();
+            this.computerNameField = new System.Windows.Forms.TextBox();
+            this.setComputerName = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -41,7 +48,7 @@ namespace Plutonium_ban_bypass
             this.groupBox1.Controls.Add(this.computerName);
             this.groupBox1.Location = new System.Drawing.Point(13, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(410, 143);
+            this.groupBox1.Size = new System.Drawing.Size(364, 143);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Current values";
@@ -66,17 +73,82 @@ namespace Plutonium_ban_bypass
             this.machineGUID.TabIndex = 1;
             this.machineGUID.Text = "Machine GUID: ";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.setComputerName);
+            this.groupBox2.Controls.Add(this.computerNameField);
+            this.groupBox2.Controls.Add(this.weed);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.newGUID);
+            this.groupBox2.Location = new System.Drawing.Point(13, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(364, 156);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Change values";
+            // 
+            // newGUID
+            // 
+            this.newGUID.AutoSize = true;
+            this.newGUID.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGUID.Location = new System.Drawing.Point(6, 25);
+            this.newGUID.Name = "newGUID";
+            this.newGUID.Size = new System.Drawing.Size(76, 16);
+            this.newGUID.TabIndex = 2;
+            this.newGUID.Text = "New GUID: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate and set new GUID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // weed
+            // 
+            this.weed.AutoSize = true;
+            this.weed.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weed.Location = new System.Drawing.Point(6, 92);
+            this.weed.Name = "weed";
+            this.weed.Size = new System.Drawing.Size(108, 16);
+            this.weed.TabIndex = 4;
+            this.weed.Text = "Computer name: ";
+            // 
+            // computerNameField
+            // 
+            this.computerNameField.Location = new System.Drawing.Point(10, 111);
+            this.computerNameField.Multiline = true;
+            this.computerNameField.Name = "computerNameField";
+            this.computerNameField.Size = new System.Drawing.Size(160, 23);
+            this.computerNameField.TabIndex = 5;
+            // 
+            // setComputerName
+            // 
+            this.setComputerName.Location = new System.Drawing.Point(177, 111);
+            this.setComputerName.Name = "setComputerName";
+            this.setComputerName.Size = new System.Drawing.Size(113, 23);
+            this.setComputerName.TabIndex = 6;
+            this.setComputerName.Text = "Set new name";
+            this.setComputerName.UseVisualStyleBackColor = true;
+            this.setComputerName.Click += new System.EventHandler(this.setComputerName_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 468);
+            this.ClientSize = new System.Drawing.Size(385, 468);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Plutonium ban bypass";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -86,6 +158,12 @@ namespace Plutonium_ban_bypass
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label computerName;
         private System.Windows.Forms.Label machineGUID;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button setComputerName;
+        private System.Windows.Forms.TextBox computerNameField;
+        private System.Windows.Forms.Label weed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label newGUID;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,7 +18,9 @@ namespace Plutonium_ban_bypass
         {
             InitializeComponent();
         }
-
+        
+        [DllImport("kernel32.dll")]
+        static extern bool SetComputerName(string lpComputerName);
 
         public string getGUID()
         {
@@ -25,10 +28,48 @@ namespace Plutonium_ban_bypass
             string r = (string)Registry.GetValue(key, "MachineGuid", (object)"default");
             return r;
         }
+        
+        public void setNewGUID(string currentGUID)
+        {
+            string key = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography";
+            Guid newGuid = Guid.NewGuid();
+            Registry.SetValue(key, (string)Registry.GetValue(key, "MachineGuid", (object)"default"), newGuid.ToString());
+            MessageBox.Show("Old GUID:" + currentGUID + "\nNew GUID: " + newGuid.ToString());
+            
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             computerName.Text += Environment.MachineName;
             machineGUID.Text += getGUID();
+        }
+        public bool setNewComputerName(string name)
+        {
+            bool success = SetComputerName(name);
+            return success;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setNewGUID(getGUID());
+        }
+
+        private void setComputerName_Click(object sender, EventArgs e)
+        {
+            string newName = computerNameField.Text;
+            if (newName == "" || newName == null) {
+                MessageBox.Show("Field can't be empty");
+            }
+            else
+            {
+                if (setNewComputerName(newName))
+                {
+                    MessageBox.Show("Changed your computer's name, make sure to reboot afterwards.");
+                }
+                else
+                {
+                    MessageBox.Show("It didn't work. You need to run this as admin!");
+                }
+            }
+
         }
     }
 }
